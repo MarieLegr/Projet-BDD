563f0744063f7d752ae72bfdae11c85b15a3195e
Marie Legrand <146454792+MarieLegr@users.noreply.github.com>
2024-02-26 13:19:20 +0100

    voici les tables

--- Projet/monappli/models.py
+++ Projet/monappli/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    emp_id = models.IntegerField()
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+class email(models.Model):
+    email = models.EmailField()
+    email_id = models.IntegerField()
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+
+class mailbody(models.Model):
+    subject = models.TextField()
+    date = models.DateTimeField()
+    content = models.TextField()
+    sender = models.ForeignKey(email, on_delete=models.CASCADE)
+
+class mailreceiver(models.Model):
+    receiver = models.EmailField()
+    mail = models.ForeignKey(mailbody, on_delete=models.CASCADE)
