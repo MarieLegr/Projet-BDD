0cce2f0bc95a55b92315e9882528a98e2bad4815
Marie Legrand <146454792+MarieLegr@users.noreply.github.com>
2024-03-01 14:25:57 +0100

    Update models.py

--- Projet/monappli/models.py
+++ Projet/monappli/models.py
@@ -1,22 +1,60 @@
 from django.db import models
-class Employee(models.Model):
+
+
+class category(models.Model):
     name = models.CharField(max_length=100)
-    emp_id = models.IntegerField()
+
+    # category_id = models.IntegerField(primary_key=True)
     def __str__(self):
         return self.name
 
+
+class Employee(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    # emp_id = models.IntegerField(primary_key=True)
+    category_fk = models.ForeignKey(category, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return f'{self.nom} {self.prenom}'
+
+
+class IntExt(models.Model):
+    internExtern = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.internExtern
+
+
 class email(models.Model):
     email = models.EmailField()
-    email_id = models.IntegerField()
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    # email_id = models.IntegerField(primary_key=True)
+    emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    id_intext = models.ForeignKey(IntExt, on_delete=models.CASCADE)
 
 
-class mailbody(models.Model):
-    subject = models.TextField()
+    def __str__(self):
+        return self.email
+
+
+
+
+class Mail(models.Model):
     date = models.DateTimeField()
+    subject = models.CharField(max_length=150)
     content = models.TextField()
     sender = models.ForeignKey(email, on_delete=models.CASCADE)
 
-class mailreceiver(models.Model):
-    receiver = models.EmailField()
-    mail = models.ForeignKey(mailbody, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.date} {self.content}'
+
+
+class receiver(models.Model):
+    receiver = models.ForeignKey(email, on_delete=models.CASCADE)
+    mail = models.ForeignKey(Mail, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+    id_intext = models.ForeignKey(IntExt, on_delete=models.CASCADE)
+
+
+
+
