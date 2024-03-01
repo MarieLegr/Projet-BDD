a8ea8375c608ae75efb82044bc480f940f9611d9
Clara <clara.guichard11@gmail.com>
2024-03-01 15:18:06 +0100

    Update models.py

--- Projet/monappli/models.py
+++ Projet/monappli/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
 
-    # category_id = models.IntegerField(primary_key=True)
     def __str__(self):
         return self.name
 
@@ -12,8 +11,7 @@ class category(models.Model):
 class Employee(models.Model):
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
-    # emp_id = models.IntegerField(primary_key=True)
-    category_fk = models.ForeignKey(category, on_delete=models.CASCADE, default=1)
+    category_id = models.ForeignKey(category, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f'{self.nom} {self.prenom}'
@@ -26,11 +24,10 @@ class IntExt(models.Model):
         return self.internExtern
 
 
-class email(models.Model):
+class Email(models.Model):
     email = models.EmailField()
-    # email_id = models.IntegerField(primary_key=True)
     emp_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    id_intext = models.ForeignKey(IntExt, on_delete=models.CASCADE)
+    intext_id = models.ForeignKey(IntExt, on_delete=models.CASCADE)
 
 
     def __str__(self):
@@ -49,11 +46,13 @@ class Mail(models.Model):
         return f'{self.date} {self.content}'
 
 
-class receiver(models.Model):
+class Receiver(models.Model):
     receiver = models.ForeignKey(email, on_delete=models.CASCADE)
-    mail = models.ForeignKey(Mail, on_delete=models.CASCADE)
+    mail_id = models.ForeignKey(Mail, on_delete=models.CASCADE)
     type = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.type
 
 
 
