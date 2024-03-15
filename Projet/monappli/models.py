b0f761db6f3d35176398be1ec36549380d31afec
Marie Legrand <146454792+MarieLegr@users.noreply.github.com>
2024-03-15 16:34:06 +0100

    voici les tables

--- Projet/monappli/models.py
+++ Projet/monappli/models.py
@@ -11,43 +11,36 @@ class Category(models.Model):
 class Employee(models.Model):
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.nom} {self.prenom}'
 
 
-class IntExt(models.Model):
-    internExtern = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.internExtern
-
-
-class Email(models.Model):
-    email = models.EmailField()
+class AdresseMail(models.Model):
+    adressemail = models.EmailField()
     employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    intext_id = models.ForeignKey(IntExt, on_delete=models.CASCADE)
+    intext_id = models.BooleanField()
 
 
     def __str__(self):
-        return self.email
+        return self.adressemail
 
 
 class Mail(models.Model):
     date = models.DateTimeField()
     subject = models.CharField(max_length=150)
     content = models.TextField()
-    sender = models.ForeignKey(Email, on_delete=models.CASCADE)
+    sender = models.ForeignKey(AdresseMail, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.date} {self.subject} {self.content}'
 
 
 class Receiver(models.Model):
-    receiver = models.ForeignKey(Email, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(AdresseMail, on_delete=models.CASCADE)
     mail_id = models.ForeignKey(Mail, on_delete=models.CASCADE)
-    type_r = models.CharField(max_length=100)
+    type_r = models.CharField(max_length=100) # To, Cc, Bcc
 
     def __str__(self):
         return self.type_r
