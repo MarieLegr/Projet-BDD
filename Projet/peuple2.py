782327da314c9ad2e6a601a939f7755f08531f1b
Clara <clara.guichard11@gmail.com>
2024-04-08 14:53:38 +0200

    Update peuple2.py

--- Projet/peuple2.py
+++ Projet/peuple2.py
@@ -25,7 +25,7 @@ pattern_contenu = re.compile(r'X-FileName:[ \S]*\n\n([\s\S]+)')
 
 
 
-def parcourir_dossier(dossier,encodage='utf-8'):
+def parcourir_dossier(dossier):
     # Parcourir tous les fichiers et dossiers dans le dossier donné
     for element in os.listdir(dossier):
         # Obtenir le chemin complet de l'élément
@@ -39,129 +39,125 @@ def parcourir_dossier(dossier,encodage='utf-8'):
             # Sinon, c'est un fichier
 
             try: 
-
-                with open(chemin_complet, 'r', encoding=encodage) as fichier:
+                with open(chemin_complet, 'r', encoding='utf-8') as fichier:
                     contenu = fichier.read()
 
-                match_receiver = re.search(pattern_receiver, contenu)
-                match_receiver2 = re.search(pattern_receiver2, contenu)
-                match_receiver3 = re.search(pattern_receiver3, contenu)
-                match_Sujet = re.search(pattern_Sujet, contenu)
-                match_date = re.search(pattern_date, contenu)
-                match_sender = re.search(pattern_sender, contenu)
-                match_contenu = re.search(pattern_contenu, contenu)
-
-
-
-
-
-                # remplissage table Mail()
-                mail = Mail()
-
-                if match_date:
-                    date_heure_str = match_date.group(1)
-                    date_heure_obj = datetime.strptime(date_heure_str, "%a, %d %b %Y %H:%M:%S")
-                    date_heure_obj = timezone.make_aware(date_heure_obj)
-                    mail.date = date_heure_obj
-
-                if match_Sujet:
-                    mail.subject = match_Sujet.group(1)
-
-                if match_contenu:
-                    mail.content = match_contenu.group(1)
-
-                if match_sender:
-
-                    adresse = AdresseMail()  
-                    ad = match_sender.group(1)  
-                    try:
-                        adresse = AdresseMail.objects.get(adressemail=ad)
-                    except :
-                        adresse.adressemail = ad
-                        adresse.intext = "@enron" in ad
-                        adresse.save()      
-
-                    mail.sender = adresse
-
-                mail.save()
-
-
-                # remplissage table Receiver()
-                if match_receiver:
-                    
-                    ad = match_receiver.group(1)
-
-                    for i in ad.split(","):
-                        i = i.strip()
-                        receiver = Receiver()
-                        receiver.mail_id = mail
-                        receiver.type_r = "To"
-
-                        adresse = AdresseMail()
-
-                        try :
-                            adresse = AdresseMail.objects.get(adressemail=i)
-                        except ObjectDoesNotExist :
-                            adresse.adressemail = i
-                            adresse.intext = "@enron" in i
-                            adresse.save()
-
-                        receiver.receiver = adresse
-                        receiver.save()
-
-
-                if match_receiver2:
-
-                    ad = match_receiver2.group(1)
-
-                    for i in ad.split(","):
-                        i = i.strip()
-                        receiver = Receiver()
-                        receiver.mail_id = mail
-                        receiver.type_r = "Cc"
-
-                        adresse = AdresseMail()
-
-                        try :
-                            adresse = AdresseMail.objects.get(adressemail=i)
-                        except ObjectDoesNotExist :
-                            adresse.adressemail = i
-                            adresse.intext = "@enron" in i
-                            adresse.save()
-
-                        receiver.receiver = adresse
-                        receiver.save()
-
-
-                if match_receiver3:
-
-                    ad = match_receiver3.group(1)
-
-                    for i in ad.split(","):
-                        i = i.strip()
-                        receiver = Receiver()
-                        receiver.mail_id = mail
-                        receiver.type_r = "Bcc"
-
-                        adresse = AdresseMail()
-
-                        try :
-                            adresse = AdresseMail.objects.get(adressemail=i)
-                        except  ObjectDoesNotExist :
-                            adresse.adressemail = i
-                            adresse.intext = "@enron" in i
-                            adresse.save()
-
-                        receiver.receiver = adresse
-                        receiver.save()
-
-
             except UnicodeDecodeError: 
-                parcourir_dossier(dossier,'ANSI')
+                with open(chemin_complet, 'r', encoding='ANSI') as fichier:
+                    contenu = fichier.read()
 
-            except :
+            except :               
                 print(f"Problème sur le fichier : {chemin_complet} \n")
 
+
+            match_receiver = re.search(pattern_receiver, contenu)
+            match_receiver2 = re.search(pattern_receiver2, contenu)
+            match_receiver3 = re.search(pattern_receiver3, contenu)
+            match_Sujet = re.search(pattern_Sujet, contenu)
+            match_date = re.search(pattern_date, contenu)
+            match_sender = re.search(pattern_sender, contenu)
+            match_contenu = re.search(pattern_contenu, contenu)
+
+
+            # remplissage table Mail()
+            mail = Mail()
+
+            if match_date:
+                date_heure_str = match_date.group(1)
+                date_heure_obj = datetime.strptime(date_heure_str, "%a, %d %b %Y %H:%M:%S")
+                date_heure_obj = timezone.make_aware(date_heure_obj)
+                mail.date = date_heure_obj
+
+            if match_Sujet:
+                mail.subject = match_Sujet.group(1)
+
+            if match_contenu:
+                mail.content = match_contenu.group(1)
+
+            if match_sender:
+
+                adresse = AdresseMail()  
+                ad = match_sender.group(1)  
+                try:
+                    adresse = AdresseMail.objects.get(adressemail=ad)
+                except :
+                    adresse.adressemail = ad
+                    adresse.intext = "@enron" in ad
+                    adresse.save()      
+
+                mail.sender = adresse
+
+            mail.save()
+
+
+            # remplissage table Receiver()
+            if match_receiver:
+                
+                ad = match_receiver.group(1)
+
+                for i in ad.split(","):
+                    i = i.strip()
+                    receiver = Receiver()
+                    receiver.mail_id = mail
+                    receiver.type_r = "To"
+
+                    adresse = AdresseMail()
+
+                    try :
+                        adresse = AdresseMail.objects.get(adressemail=i)
+                    except ObjectDoesNotExist :
+                        adresse.adressemail = i
+                        adresse.intext = "@enron" in i
+                        adresse.save()
+
+                    receiver.receiver = adresse
+                    receiver.save()
+
+
+            if match_receiver2:
+
+                ad = match_receiver2.group(1)
+
+                for i in ad.split(","):
+                    i = i.strip()
+                    receiver = Receiver()
+                    receiver.mail_id = mail
+                    receiver.type_r = "Cc"
+
+                    adresse = AdresseMail()
+
+                    try :
+                        adresse = AdresseMail.objects.get(adressemail=i)
+                    except ObjectDoesNotExist :
+                        adresse.adressemail = i
+                        adresse.intext = "@enron" in i
+                        adresse.save()
+
+                    receiver.receiver = adresse
+                    receiver.save()
+
+
+            if match_receiver3:
+
+                ad = match_receiver3.group(1)
+
+                for i in ad.split(","):
+                    i = i.strip()
+                    receiver = Receiver()
+                    receiver.mail_id = mail
+                    receiver.type_r = "Bcc"
+
+                    adresse = AdresseMail()
+
+                    try :
+                        adresse = AdresseMail.objects.get(adressemail=i)
+                    except ObjectDoesNotExist :
+                        adresse.adressemail = i
+                        adresse.intext = "@enron" in i
+                        adresse.save()
+
+                    receiver.receiver = adresse
+                    receiver.save()
 
 
 parcourir_dossier(dossier)
