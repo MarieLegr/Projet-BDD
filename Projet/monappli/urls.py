f79f7f729e71c24c053e966024c31478137d6e1d
Marie Legrand <146454792+MarieLegr@users.noreply.github.com>
2024-04-27 23:36:45 +0200

    template

--- Projet/monappli/urls.py
+++ Projet/monappli/urls.py
@@ -2,14 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('index1', views.index1, name='index1'),
     path('index', views.index, name='index'),
     path('autre_page', views.vue2, name='vue2'),
     path('Employee', views.vue3, name='vue3'),
-    path('', views.index, name='vide'),
+    path('', views.index1, name='vide'), #REMPLACER index1 PAR index
     path('search_employee', views.search_employee, name='search_employee'),
     path('vue5', views.vue5, name='vue5'),
     path('test', views.vue4, name='vue4'),
     path('vue6', views.vue6, name='vue6'),
     path('search_employee3', views.search_employee3, name='search_employee3'),
+    path('request4', views.request4, name='request4'),
+    path('couples_employes', views.couples_employes, name='couples_employes'),
+    path('requete1', views.requete1, name='requete1'),
+    path('attributs_employes', views.attributs_employes, name='attributs_employes'),
+    path('requete2', views.requete2, name='requete2'),
+    path('employeXmail', views.employeXmail , name='employeXmail'),
+    path('requete3', views.requete3, name='requete3'),
+    path('List_comu_empl', views.List_comu_empl, name='List_comu_empl'),
+    path('requete5', views.requete5, name='requete5'),
+    path('PgdNBmail', views.PgdNBmail, name='PgdNBmail'),
 
 ]
--- Projet/monappli/views.py
+++ Projet/monappli/views.py
@@ -9,13 +9,15 @@ from .forms import SearchForm
 from django.db import connection
 
 
+def index(request):
+    return render(request, "index.tmpl")
 
 # Create your views here.
-def index(request):
+def index1(request):
     employees=Employee.objects.all()
     #category=Category.objects.all()
     context={"employees":employees}
-    return render(request,"index.tmpl",context=context)
+    return render(request,"index1.tmpl",context=context)
 
 def vue2(request):
     return HttpResponse("<p>Cette vue est une page d'aide basique pour l'application de mon projet.</p>") #<p> est un paragraphe en HTML
@@ -40,7 +42,7 @@ def search_employee2(request):
             return render(request, "employee.tmpl", context=context)
     else:
         form = SearchForm()
-    return render(request, 'index.tmpl', {'form': form})
+    return render(request, 'index1.tmpl', {'form': form})
 
 
 def search_employee(request):
@@ -53,14 +55,14 @@ def search_employee(request):
             category=Category.objects.get(id=employe.category_id_id)
         return render(request, 'employee.tmpl', {'employe': employe, 'category': category})
     else:
-        return render(request,'index.tmpl')
+        return render(request,'index1.tmpl')
 
 
 def vue4(request):
     employee = Employee.objects.all()
     category = Category.objects.all()
     context = {"employee": employee, "category": category}
-    return render(request, "index.tmpl", context=context)
+    return render(request, "index1.tmpl", context=context)
 
 
 def vue5(request):
@@ -69,7 +71,7 @@ def vue5(request):
 
         return render(request, 'employee.tmpl', {'employe': employe})
     else:
-        return render(request, 'index.tmpl')
+        return render(request, 'index1.tmpl')
 
 
 #requête sql
@@ -78,7 +80,7 @@ def vue5(request):
 def vue6(request):
     employees = Employee.objects.raw('SELECT * FROM monappli_employee where category_id_id is not null')
     context = {"employees": employees}
-    return render(request, "index.tmpl", context=context)
+    return render(request, "index1.tmpl", context=context)
 
 
 #même chose que search_employee mais avec une requête sql
@@ -111,4 +113,413 @@ def search_employee3(request):
             return render(request, 'employee_details.tmpl', context)
 
     else:
-        return render(request, 'index.tmpl')
+        return render(request, 'index1.tmpl')
+
+
+#couples d’employés ayant communiqué dans un intervalle de temps choisi (liste
+#ordonnée suivant le nombre de mails échangés, tronquée au-dessous d’un seuil
+#paramétrable) ;
+
+
+
+def request4(request):
+    return render(request, 'requete4.tmpl',
+                  {
+
+                  })
+
+
+def couples_employes(request):
+    if request.method == 'POST':
+        date_debut = request.POST.get('date_debut')
+        date_fin = request.POST.get('date_fin')
+        seuil = request.POST.get('seuil')
+
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT DISTINCT
+    CASE
+        WHEN e.nom <= f.nom THEN e.nom
+        ELSE f.nom
+    END AS nom1,
+
+    CASE
+        WHEN e.prenom <= f.prenom THEN e.prenom
+        ELSE f.prenom
+    END AS prenom1,
+
+    CASE
+        WHEN e.nom <= f.nom THEN f.nom
+        ELSE e.nom
+    END AS nom2,
+
+    CASE
+        WHEN e.prenom <= f.prenom THEN f.prenom
+        ELSE e.prenom
+    END AS prenom2,
+    COUNT(*) AS total_echanges
+FROM
+    monappli_mail m
+    INNER JOIN monappli_receiver r ON m.id = r.mail_id_id
+    INNER JOIN monappli_adressemail a ON m.sender_id = a.id
+    INNER JOIN monappli_adressemail b ON r.receiver_id = b.id
+    INNER JOIN monappli_employee e ON a.employee_id_id = e.id
+    INNER JOIN monappli_employee f ON b.employee_id_id = f.id
+WHERE
+    date BETWEEN %s AND %s
+GROUP BY
+    nom1, prenom1, nom2,  prenom2
+HAVING
+    COUNT(*) > %s
+ORDER BY
+    total_echanges DESC;
+            """, [date_debut, date_fin, seuil])
+            rows = cursor.fetchall()
+
+        couples = []
+        for row in rows:
+            nom1, prenom1, nom2, prenom2, nb_mails = row
+            couples.append({
+                'nom1': nom1,
+                'prenom1': prenom1,
+                'nom2': nom2,
+                'prenom2': prenom2,
+                'nb_mails': nb_mails
+            })
+
+        context = {
+            'couples': couples,
+            'date_debut': date_debut,
+            'date_fin': date_fin,
+            'seuil': seuil
+        }
+        return render(request, 'couples_employes.tmpl', context)
+
+
+
+def requete1(request):
+    return render(request, 'requete1.tmpl',
+                  {
+
+                  })
+
+def attributs_employes(request):
+    if request.method == 'POST':
+        nom_employe = request.POST.get('nom_employe')
+        prenom_employe = request.POST.get('prenom_employe')
+        adresse_employe = request.POST.get('adresse_employe')
+
+        if adresse_employe == "":
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    SELECT e.nom, e.prenom, c.name, am.adressemail
+                    FROM monappli_employee e
+                    LEFT JOIN monappli_category c ON e.category_id_id = c.id
+                    LEFT JOIN monappli_adressemail am ON e.id = am.employee_id_id
+                    WHERE e.nom = %s AND e.prenom = %s;
+                """, [nom_employe, prenom_employe])
+                rows = cursor.fetchall()
+
+            if rows:
+                coupleadress=[]
+                for row in rows:
+                    nom_employe, prenom_employe, categorie, adresse_employe = row
+                    coupleadress.append(adresse_employe)
+
+
+                context = {
+                    'nom_employe': nom_employe,
+                    'prenom_employe': prenom_employe,
+                    'categorie': categorie,
+                    'coupleadress': coupleadress
+                }
+                return render(request, 'attributs_employes.tmpl', context)
+            else:
+                # Gérer le cas où aucun résultat n'est trouvé pour l'adresse email
+                return HttpResponse("Aucun employé trouvé pour cette adresse email.")
+
+        else:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    SELECT e.nom, e.prenom, c.name, am.adressemail 
+                    FROM monappli_employee e
+                    LEFT JOIN monappli_category c ON e.category_id_id = c.id
+                    LEFT JOIN monappli_adressemail am ON e.id = am.employee_id_id
+                    WHERE e.id IN (SELECT employee_id_id FROM monappli_adressemail WHERE adressemail = %s);
+                """, [adresse_employe])
+                rows = cursor.fetchall()
+
+            if rows:
+                coupleadress=[]
+                for row in rows:
+                    nom_employe, prenom_employe, categorie, adresse_employe = row
+                    coupleadress.append({
+                        'adresse_employe': adresse_employe
+                    })
+
+
+                context = {
+                    'nom_employe': nom_employe,
+                    'prenom_employe': prenom_employe,
+                    'categorie': categorie,
+                    'coupleadress': coupleadress
+                }
+                return render(request, 'attributs_employes.tmpl', context)
+            else:
+                # Gérer le cas où aucun résultat n'est trouvé pour l'adresse email
+                return HttpResponse("Aucun employé trouvé pour cette adresse email.")
+
+  
+
+def requete2(request):
+    return render(request, 'requete2.tmpl',
+                  {
+
+                  })
+
+
+def employeXmail(request):
+    if request.method == 'POST':
+        date_debut = request.POST.get('date_debut')
+        date_fin = request.POST.get('date_fin')
+        seuil = request.POST.get('seuil')
+
+        with connection.cursor() as cursor:
+            cursor.execute("""select e.nom, e.prenom,nb_mails_envoyes from monappli_employee e
+inner join (select monappli_adressemail.employee_id_id, nb_mails_envoyes from monappli_adressemail
+inner join
+    (select sender_id, count(*) as nb_mails_envoyes
+    from monappli_mail
+    where date between %s and %s
+    group by sender_id
+    having count(*) > %s) as envoyes on envoyes.sender_id = monappli_adressemail.id
+    where monappli_adressemail.employee_id_id is not null) as envoie2 on e.id = envoie2.employee_id_id;""", [date_debut, date_fin, seuil])
+            rows1 = cursor.fetchall()
+
+
+            liste1=[]
+            for row in rows1:
+                nom, prenom, nb_mails_envoyes = row
+                liste1.append({
+                    'nom': nom,
+                    'prenom': prenom,
+                    'nb_mails_envoyes': nb_mails_envoyes
+                })
+
+
+        with connection.cursor() as cursor:
+            cursor.execute("""SELECT t.nom, t.prenom, SUM(recus.nb_mails_recus) AS total_mails_recus
+FROM (
+    SELECT e.nom, e.prenom, a.id
+    FROM monappli_employee e
+    INNER JOIN monappli_adressemail a ON e.id = a.employee_id_id
+) AS t
+INNER JOIN (
+    SELECT r.receiver_id, COUNT(*) AS nb_mails_recus
+    FROM monappli_receiver r
+    INNER JOIN monappli_mail m ON r.mail_id_id = m.id
+    WHERE m.date BETWEEN %s AND %s AND r.type_r != 'Bcc'
+    GROUP BY r.receiver_id
+    HAVING COUNT(*) > %s
+) AS recus ON recus.receiver_id = t.id
+GROUP BY t.nom, t.prenom;""", [date_debut, date_fin, seuil])
+            rows2 = cursor.fetchall()
+
+            liste2 = []
+            for row in rows2:
+                nom, prenom, nb_mails_envoyes = row
+                liste2.append({
+                    'nom': nom,
+                    'prenom': prenom,
+                    'nb_mails_envoyes': nb_mails_envoyes
+                })
+
+
+
+            context = {
+                'liste1': liste1,
+                'liste2': liste2,
+                'date_debut': date_debut,
+                'date_fin': date_fin,
+                'seuil': seuil
+            }
+            return render(request, 'employeXmail.tmpl', context)
+    else:
+        return render(request, 'requete2.tmpl')
+
+
+
+def requete3(request):
+    employees = Employee.objects.all()
+    # category=Category.objects.all()
+    context = {"employees": employees}
+    return render(request, "requete3.tmpl", context=context)
+
+
+def List_comu_empl(request):
+    if request.method =='POST':
+        id = request.POST.get('employe')
+        employe = Employee.objects.get(id=id)
+        employe_nom = employe.nom
+        employe_prenom = employe.prenom
+        date_debut = request.POST.get('date_debut')
+        date_fin = request.POST.get('date_fin')
+
+        with connection.cursor() as cursor:
+            cursor.execute("""SELECT DISTINCT e_receiver.nom ,e_receiver.prenom, count(e_receiver.id)
+FROM monappli_employee e
+INNER JOIN monappli_adressemail am ON e.id = am.employee_id_id
+INNER JOIN monappli_mail m ON am.id = m.sender_id
+INNER JOIN monappli_receiver r ON m.id = r.mail_id_id
+INNER JOIN monappli_adresseMail am_receiver ON r.receiver_id = am_receiver.id
+INNER JOIN monappli_employee e_receiver ON am_receiver.employee_id_id = e_receiver.id
+WHERE 
+    (e.nom = %s AND e.prenom = %s)
+    AND m.date BETWEEN %s AND %s AND r.type_r != 'Bcc'
+GROUP BY e_receiver.nom ,e_receiver.prenom;""", [employe_nom, employe_prenom, date_debut, date_fin])
+
+            rows = cursor.fetchall()
+
+            liste1 = []
+            for row in rows:
+                nom, prenom, nb_mails = row
+                liste1.append({
+                    'nom': nom,
+                    'prenom': prenom,
+                    'nb_mails': nb_mails
+                })
+
+        with connection.cursor() as cursor:
+            cursor.execute("""SELECT DISTINCT e_receiver.nom ,e_receiver.prenom, count(e_receiver.id)
+FROM monappli_employee e
+INNER JOIN monappli_adressemail am ON e.id = am.employee_id_id
+INNER JOIN monappli_receiver r ON am.id = r.receiver_id
+INNER JOIN monappli_mail m ON r.mail_id_id = m.id
+INNER JOIN monappli_adresseMail am_receiver ON m.sender_id = am_receiver.id
+INNER JOIN monappli_employee e_receiver ON am_receiver.employee_id_id = e_receiver.id
+WHERE 
+    (e.nom = %s AND e.prenom = %s)
+    AND m.date BETWEEN %s AND %s AND r.type_r != 'Bcc'
+GROUP BY e_receiver.nom ,e_receiver.prenom;""", [employe_nom, employe_prenom, date_debut, date_fin])
+
+            rows2 = cursor.fetchall()
+
+            liste2 = []
+            for row in rows2:
+                nom, prenom, nb_mails = row
+                liste2.append({
+                    'nom': nom,
+                    'prenom': prenom,
+                    'nb_mails': nb_mails
+                })
+
+        context = {
+            'liste1': liste1,
+            'liste2': liste2,
+            'employe_nom': employe_nom,
+            'employe_prenom': employe_prenom,
+            'date_debut': date_debut,
+            'date_fin': date_fin
+        }
+        return render(request, 'List_comu_empl.tmpl', context)
+    else:
+        return render(request, 'requete3.tmpl')
+
+
+
+def requete5(request):
+    return render(request, 'requete5.tmpl',
+                  {
+
+                  })
+
+def PgdNBmail(request):
+    if request.method == 'POST':
+        date_debut = request.POST.get('date_debut')
+        date_fin = request.POST.get('date_fin')
+        echange = request.POST.get('IntExt')
+
+        if echange == 'Interne/Interne':
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT
+    DATE_TRUNC('day', m.date) AS jour,
+    COUNT(*) AS nombre_de_mails
+FROM monappli_mail m
+INNER JOIN monappli_receiver r ON m.id = r.mail_id_id
+inner join monappli_adressemail a on m.sender_id = a.id
+inner join monappli_adressemail b on r.receiver_id = b.id
+WHERE m.date BETWEEN '2000-12-13' AND '2001-01-01' and a.intext = true and b.intext = true and r.type_r != 'Bcc'
+GROUP BY jour
+ORDER BY nombre_de_mails DESC;""", [date_debut, date_fin])
+                rows = cursor.fetchall()
+
+                liste = []
+                for row in rows:
+                    jour, nb_mails = row
+                    liste.append({
+                        'jour': jour,
+                        'nb_mails': nb_mails
+                    })
+
+        elif echange == 'IE':
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT
+    DATE_TRUNC('day', m.date) AS jour,
+    COUNT(*) AS nombre_de_mails
+FROM monappli_mail m
+INNER JOIN monappli_receiver r ON m.id = r.mail_id_id
+inner join monappli_adressemail a on m.sender_id = a.id
+inner join monappli_adressemail b on r.receiver_id = b.id
+WHERE m.date BETWEEN '2000-12-13' AND '2001-01-01' and ((a.intext = false and b.intext = true) or (a.intext = true and b.intext = false)) and r.type_r != 'Bcc'
+GROUP BY jour
+ORDER BY nombre_de_mails DESC;""", [date_debut, date_fin])
+                rows = cursor.fetchall()
+
+                liste = []
+                for row in rows:
+                    jour, nb_mails = row
+                    liste.append({
+                        'jour': jour,
+                        'nb_mails': nb_mails
+                    })
+
+        else :
+            with connection.cursor() as cursor:
+                cursor.execute("""SELECT
+    DATE_TRUNC('day', m.date) AS jour,
+    COUNT(*) AS nombre_de_mails
+FROM monappli_mail m
+INNER JOIN monappli_receiver r ON m.id = r.mail_id_id
+inner join monappli_adressemail a on m.sender_id = a.id
+inner join monappli_adressemail b on r.receiver_id = b.id
+WHERE m.date BETWEEN '2000-12-13' AND '2001-01-01' and ((a.intext = false and b.intext = true) or (a.intext = true and b.intext = false) or (a.intext = true and b.intext = true )) and r.type_r != 'Bcc'
+GROUP BY jour
+ORDER BY nombre_de_mails DESC;""", [date_debut, date_fin])
+
+                rows = cursor.fetchall()
+
+                liste = []
+                for row in rows:
+
+                    jour, nb_mails = row
+                    liste.append({
+                        'jour': jour,
+                        'nb_mails': nb_mails
+                    })
+
+        context = {
+            'liste': liste,
+            'date_debut': date_debut,
+            'date_fin': date_fin,
+            'echange': echange
+        }
+
+        return render(request, 'PgdNBmail.tmpl', context)
+    else:
+        return render(request, 'requete5.tmpl')
+
+        
+        
+    
+
+
